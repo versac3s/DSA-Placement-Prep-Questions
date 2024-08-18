d15322a12edcd2e15a7c2f901479c58b496402b0
versac3s <clinquantscorp12sonder@gmail.com>
2024-08-18 14:12:32 +0530

    Update

---
+++ Graph4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Graph4 {
+    static class Edge{
+        int src, dest;
+
+        public Edge(int s, int d){
+            this.src=s;
+            this.dest=d;
+        }
+    }
+    //CYCLE DETECTION
+
+    //In Undirected Graph
+    public static void dfs(ArrayList<Edge>[] graph){
+        
+
+    }
+    
+}
--- Graphs3.java
+++ Graphs3.java
@@ -18,9 +18,21 @@ public class Graphs3 {
     //Thus, it might not be possible for us to traverse the graph with single source.
     //So we modify the previous traversal strategy.
 
+    //We make a new helping function, which stores all the visited[curr] as false.
+    //We start with source, call the bfs/dfsUtil function, traverse and make the vis[curr]=true.
+    //Then we see for which node is the vis[curr] = false. 
+    //We go to that node and then start again with the traversal.
+
     public static void bfs(ArrayList<Edge>[] graph){
-        Queue<Integer> q = new LinkedList<>();
         boolean visited[] = new boolean[graph.length];
+        for(int i=0; i<graph.length;i++){
+            if(!visited[i]){
+                bfsUtil(graph, visited);
+            }
+        }
+    }
+    public static void bfsUtil(ArrayList<Edge>[] graph, boolean visited[]){
+        Queue<Integer> q = new LinkedList<>();
         q.add(0); //source
         while(!q.isEmpty()){
             int curr = q.remove();
@@ -31,6 +43,24 @@ public class Graphs3 {
                     Edge e = graph[curr].get(i);
                     q.add(e.dest);
                 }
+            }
+        }
+    }
+
+    public static void dfs(ArrayList<Edge>[] graph){
+        boolean visited[]= new boolean[graph.length];
+        for(int i=0; i<graph.length; i++){
+            dfsUtil(graph, i, visited);
+        }
+    }
+
+    public static void dfsUtil(ArrayList<Edge>[] graph, int curr, boolean visited[]){
+        System.out.println(curr);
+        visited[curr]=true;
+        for(int i=0; i<graph[curr].size(); i++){
+            Edge e = graph[curr].get(i);
+            if(!visited[e.dest]){
+                dfsUtil(graph, e.dest, visited);
             }
         }
     }
